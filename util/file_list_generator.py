742358c3b975524e6253d93338482be14c1e5d3f
JiashengWU <js_wuuuu@hotmail.com>
2018-10-11 12:10:32 -0700

    Upload start code

---
+++ util/file_list_generator.py
@@ -0,0 +1,33 @@
+########################################
+# Jiasheng Wu
+# Sep 26, 2018
+########################################
+
+folder_path = '/Users/wjs-mbp/Downloads/den_downloader'
+
+input_path = folder_path + '/input.txt'
+output_path1 = folder_path + '/util/file_list.txt'
+output_path2 = folder_path + '/util/output_name.txt'
+
+data = []
+
+with open(input_path, mode='r') as input_file:
+    for line in input_file:
+        line = line.strip().strip('\n')
+        if not line == '' and not line.startswith('#'):
+            # output_file.write(line + '\n')
+            data.append(line)
+    # print(data)
+
+url_prefix = data[0]
+ts_file_num = int(data[1])
+
+with open(output_path1, mode='w') as output_file:
+    for i in range(1, ts_file_num + 1):
+        # print("file '" + url_prefix + str(i) + ".ts'")
+        output_file.write("file '" + url_prefix + str(i) + ".ts'\n")
+
+output_name = data[2]
+
+with open(output_path2, mode='w') as output_file:
+    output_file.write(output_name)
